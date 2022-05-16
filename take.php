930cbe9404e0907166b1cf941c9a8cf0c9285e45
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2022-05-16 22:06:51 +0300

    ...

--- take.php
+++ take.php
@@ -2,13 +2,26 @@
 
 $data = file_get_contents("https://github.com/YanikFatih");
 
-preg_match_all('<span class="text-bold color-fg-default">2</span>(.*?) (.*?)followers@si', $data, $dlinktake);
+preg_match_all('<span class="text-bold color-fg-default">2</span>(.*?) (.*?)"followers"@si', $data, $dlinktake);
 
-echo "<pre>";
-print_r($dlinktake);
-echo "</pre>";
-echo "<br>";
+//print_r($dlinktake);
 
-echo $total = $dlinktake[2][0];
+$total = $dlinktake[2][0];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Github Follower Count Tracker</title>
+    <meta charset="UTF-8">
+    <meta http_equiv="refresh" content="300;URL=take.php">
+</head>
+<body>
+    <br>
+    <br>
+    <b style="font: size 400px;"><center><?php echo $total ?></center></b>
+    
+
+</body>
+</html>
