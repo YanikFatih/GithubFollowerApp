cf696f74db288c26d15f7972d43cbda779bd04da
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2022-05-19 22:22:20 +0300

    follower sound added

--- take.php
+++ take.php
@@ -44,7 +44,7 @@ $total = $dlinktake[1][0];
     
     <h1 style="font-size:100px; color:green;" ><center>NEW FOLLOWER!</center></h1>
 
-    <audio src=""></audio>
+    <audio src="follower.mp3" type="audio/mpeg">Your browser does not support the audio element</audio>
 
 </body>
 </html>
